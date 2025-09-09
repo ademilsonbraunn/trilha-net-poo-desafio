1af5baa17760985c7c353d9b3ead6349e95d06a7
ADEMILSON\ademison.braunn <ademilsondbraunn@gmail.com>
2025-09-09 20:28:52 -0300

    Correção do Projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("99999-9999", "Nokia Tijolão", "111111", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+Console.WriteLine("-----------------------------");
+
+Iphone iphone = new Iphone("88888-8888", "iPhone 14 Pro", "222222", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
